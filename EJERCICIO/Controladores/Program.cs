9157e234a1c1debf5372a27bb641a623fc8c100f
DESKTOP-T3B5TCQ\David <csi23-dmunnoz@colegioaltair.net>
2023-10-30 20:06:28 +0100

    Agregar archivos de proyecto.

---
+++ EJERCICIO/Controladores/Program.cs
@@ -0,0 +1,31 @@
+﻿using EJERCICIO.Servicios;
+
+namespace EJERCICIO.Controladores
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            MenuInterfaz mi = new MenuImplementacion();
+            OperacionInterfaz oi = new OperacionImplementacion();
+            {
+                bool cerrarMenu = false;
+                int opcionIntroducida;
+                while (!cerrarMenu)
+                {
+                    opcionIntroducida = mi.mostrarMenuYSeleccion();
+                    switch (opcionIntroducida)
+                    {
+                        case 0:
+                            cerrarMenu |= true;
+                            break;
+                            case 1:
+                            int solicita = oi.solicitar();
+                            break;
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EJERCICIO/Servicios/OperacionImplementacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO.Servicios
+{
+    internal class OperacionImplementacion : OperacionInterfaz
+    {
+        public int solicitar(int numero)
+        {
+            int numeroInt = Convert.ToInt32(Console.ReadLine());
+            {
+                if (numeroInt%2 == 0)
+                {
+                    Console.WriteLine("es par");
+                }
+                else;
+                {
+                    Console.WriteLine("es impar");
+                }
+            }
+
+        }
+    }
+}
